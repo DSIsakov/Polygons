2bf0d5c59245047cdeff1da8b0441fb021c1b1cf
DIsakov <55271574+DIsakov14504@users.noreply.github.com>
2020-11-04 19:28:15 +0300

    Add files via upload

---
+++ WindowsFormsApp5/Class1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp5
+{
+    abstract class Node
+    {
+        protected int x0;
+        protected int y0;
+        public static int r { get; set; }
+        public static Color color { get; set; }
+        public Node(int x, int y)
+        {
+            x0 = x;
+            y0 = y;
+        }
+        static Node()
+        {
+            r = 50;
+            color = Color.Black;
+        }
+        public int SetX 
+        { 
+            get { return x0; } 
+            set { x0 = value; } 
+        }
+        public int SetY
+        { 
+            get { return y0; } 
+            set { y0 = value; } 
+        }
+        abstract public void DrawNode(Graphics g);
+        abstract public bool Check(int x, int y);
+    }
+    class Triangle : Node
+    {
+        public Triangle(int x, int y) : base(x, y) { }
+        public override void DrawNode(Graphics g)
+        {
+            Point point1 = new Point(x0 - r/2, y0 + r/2);
+            Point point2 = new Point(x0 + r/2, y0 + r/2);
+            Point point3 = new Point(x0, y0 - r/2);
+            Point[] points = { point1, point2, point3 };
+            g.FillPolygon(new SolidBrush(color), points);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (x0 - (r / 2) < x && x0 + (r / 2) > x && y0 - r < y && y0 + (r / 2) > y) return true;
+            else return false;
+        }
+    }
+    class Circle : Node
+    {
+        public Circle(int x, int y) : base(x, y) { }
+        public override void DrawNode(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(color), x0 - (r / 2), y0 - (r / 2), r, r);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (Math.Sqrt(Math.Pow(x - x0 - (r / 2), 2) + Math.Pow(y - y0 - (r / 2), 2)) < r) return true;
+            else return false;
+        }
+    }
+    class Square : Node
+    {
+        public Square(int x, int y) : base(x, y) { }
+        public override void DrawNode(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(color), x0 - (r / 2), y0 - (r / 2), r, r);
+        }
+        public override bool Check(int x, int y)
+        {
+            if (x0 - (r / 2) < x && x0 + (r / 2) > x && y0 - (r / 2) < y && y0 + (r / 2) > y) return true;
+            else return false;
+        }
+    }
+}
---
+++ WindowsFormsApp5/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        bool draw;
+        bool drag;
+        int dx;
+        int dy;
+        int nShape;
+        Node node;
+        public Form1()
+        {
+            InitializeComponent();
+            draw = false;
+            drag = false;
+            dx = 0;
+            dy = 0;
+            nShape = 0;
+        }
+        private void Form1_Load(object sender, EventArgs e) { DoubleBuffered = true; }
+        private void triangleToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 0; }
+        private void circleToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 1; }
+        private void squareToolStripMenuItem_Click(object sender, EventArgs e) { nShape = 2; }
+        private void Form1_Paint(object sender, PaintEventArgs e) { if (draw) node.DrawNode(e.Graphics); }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (draw && node.Check(e.X, e.Y) && e.Button == MouseButtons.Left)
+            {
+                dx = e.X - node.SetX;
+                dy = e.Y - node.SetY;
+                drag = true;
+            } 
+            else if (draw && node.Check(e.X, e.Y) && e.Button == MouseButtons.Right)
+            {
+                draw = false;
+                node = null;
+                Refresh();
+            }
+            else
+            {
+                draw = true;
+                switch (nShape)
+                {
+                    case 0:
+                        node = new Triangle(e.X, e.Y);
+                        break;
+                    case 1:
+                        node = new Circle(e.X, e.Y);
+                        break;
+                    case 2:
+                        node = new Square(e.X, e.Y);
+                        break;
+                }
+            }
+        }
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (drag)
+            {
+                node.SetX = e.X - dx;
+                node.SetY = e.Y - dy;
+                Refresh();
+            }
+        }
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drag = false;
+            Refresh();
+        }
+    }
+}
